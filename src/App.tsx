2b54a92ae4bd3cdef62f0bb4bb1a15f4a06a488a
James Errington <james@maprunner.co.uk>
2020-10-15 00:01:53 +0100

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import type { FunctionComponent } from "react"
+
+import "./App.css"
+
+export const App: FunctionComponent = () => {
+  return <div className="App"></div>
+}
